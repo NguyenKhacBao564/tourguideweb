89188c3cfbe6f6fa6c18cfc04d9e199deaac3de1
biin284 <anhn162004@gmail.com>
2025-04-15 17:43:57 +0700

    chua lay code ve

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Page from "./pages/User/Home";
 import Register from "./pages/User/Register";
@@ -9,25 +9,26 @@ import Contact from "./pages/User/Contact";
 import TourManagement from "./pages/Admin/Employee_Bussiness/TourManagement";
 import Employee_Bussiness from "./pages/Admin/Employee_Bussiness/Employee_Bussiness";
 import AddTourArea from "./components/AddTourArea/AddTourArea";
+import TourHistory from "./pages/User/TourHistory";
 
 function App() {
   return (
     <div className="App">
-        <ScrollToTop /> 
-        <Routes>
-          <Route path="/" element={<Page /> } />
-          <Route path="/contact" element={<Contact/> } />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/addtour" element={<AddTourArea/>} />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Page />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/addtour" element={<AddTourArea />} />
 
-          <Route path="/admin" element={<Employee_Bussiness/>}>
-              <Route path="khachhang" element={<p>Khách hàng</p>} />
-              <Route path="lichdat" element={<TourManagement/>} />
-              <Route path="khuyenmai" element={<p>Khuyến mãi</p>} />
-          </Route>
-          
-        </Routes>
+        <Route path="/admin" element={<Employee_Bussiness />}>
+          <Route path="khachhang" element={<p>Khách hàng</p>} />
+          <Route path="lichdat" element={<TourManagement />} />
+          <Route path="khuyenmai" element={<p>Khuyến mãi</p>} />
+        </Route>
+
+      </Routes>
     </div>
   );
 }
