d5a252aadda522e95095df22bf8e341e00729888
Duy <you@example.com>
2025-04-15 17:19:55 +0700

    update con thieu

---
+++ src/pages/User/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Maincontent from '../../layouts/MainContent';
+import Header from '../../layouts/Header';
+import Footer from '../../layouts/Footer';
+import { Link } from 'react-router-dom';
+
+
+function Page(props) {
+    
+    return (
+        <div>
+          <Header/>
+          <Maincontent/>
+          <Link to="/admin/managetour">Admin</Link>
+          <Footer/>
+        </div>
+    );
+}
+
+export default Page;
