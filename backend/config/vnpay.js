12cc3067e0bd07a7a62a877ee6b65525f0ba0b45
Nguyen Khac Bao <onekingdom564@gmail.com>
2025-06-06 13:10:22 +0700

    deploy payment test

--- backend/config/vnpay.js
+++ backend/config/vnpay.js
@@ -3,8 +3,8 @@ const vnpayConfig = {
   vnp_HashSecret: 'RB6ZXCA2SAJEN29WMBOFRUGP5HNTOKBG', // Secret Key từ thông tin bạn cung cấp
   vnp_Url: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html', // URL thanh toán test
   vnp_Api: 'https://sandbox.vnpayment.vn/merchant_webapi/api/transaction',
-  vnp_ReturnUrl: 'http://localhost:3001/api/payment/vnpay-return', // URL return về backend để xử lý
-  vnp_IpnUrl: 'http://localhost:3001/api/payment/vnpay-ipn', // URL IPN callback
+  vnp_ReturnUrl: 'http://100.90.83.88:3001/api/payment/vnpay-return', // URL return về backend để xử lý
+  vnp_IpnUrl: 'http://100.90.83.88:3001/api/payment/vnpay-ipn', // URL IPN callback
   vnp_Version: '2.0.0',
   vnp_Command: 'pay',
   vnp_CurrCode: 'VND',
